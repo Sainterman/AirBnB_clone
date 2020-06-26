c2fb273222a7972c6aaceaaeb958318be870f41d
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-26 03:44:59 +0000

    add models directory and class basemodel, __init__

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class BaseModel that defines
+    all common attributes/methods
+    for other classes:
+"""
+
+
+class BaseModel:
+    """
+    """
+
+    def __init__(self):
+        self.uuid = id
+        
